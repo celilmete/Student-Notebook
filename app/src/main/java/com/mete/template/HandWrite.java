6fdee04fb75155396123a22beb82986203fad4ab
celilmete <celilmete1997>
2020-04-22 16:39:07 +0300

    first commit

---
+++ app/src/main/java/com/mete/template/HandWrite.java
@@ -0,0 +1,38 @@
+package com.mete.template;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class HandWrite extends AppCompatActivity {
+    PaintView paintView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hand_write);
+
+        paintView = findViewById(R.id.view);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.clear) {
+            paintView.clear();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
