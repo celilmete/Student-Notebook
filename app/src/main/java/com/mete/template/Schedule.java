f6a2fc378208c0296230b26f0c437e599092d1ee
celilmete <celilmete1997>
2020-05-07 18:53:45 +0300

    updates

--- app/src/main/java/com/mete/template/Schedule.java
+++ app/src/main/java/com/mete/template/Schedule.java
@@ -1,17 +1,21 @@
 package com.mete.template;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import java.util.zip.Inflater;
 
 public class Schedule extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_schedule);;
+        setContentView(R.layout.activity_schedule);
     }
 
     @Override
@@ -19,5 +23,20 @@ public class Schedule extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_schedule,menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+
+            case R.id.edit_schedule :
+                setContentView(R.layout.edit_schedule);
+                    break;
+            case R.id.save_schedule :
+
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
