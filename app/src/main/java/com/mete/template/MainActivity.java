c77fb7ec7c18f1566e778ea46c585fc7dca84ad5
Misa <isa.akbaba03@gmail.com>
2020-05-27 22:41:23 +0300

    upload beta has added

--- app/src/main/java/com/mete/template/MainActivity.java
+++ app/src/main/java/com/mete/template/MainActivity.java
@@ -1,10 +1,15 @@
 package com.mete.template;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,18 +17,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        checkPermission();
     }
 
     public void takeNote(View view) {
-        startActivity(new Intent(this,HandWrite.class));
+        startActivity(new Intent(this, HandWrite.class));
     }
 
     public void tasks(View view) {
-        startActivity(new Intent(this,Todo.class));
+        startActivity(new Intent(this, Todo.class));
     }
 
     public void schedule(View view) {
-        startActivity(new Intent(this,Schedule.class));
+        startActivity(new Intent(this, Schedule.class));
     }
 
+    public void uploadPhoto(View view) {
+        startActivity(new Intent(this, UploadPhoto.class));
+    }
+
+    public void checkPermission() {
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                == PackageManager.PERMISSION_DENIED) {
+
+            // Requesting the permission
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                    1);
+        } else {
+            Toast.makeText(MainActivity.this,
+                    "Permission already granted",
+                    Toast.LENGTH_SHORT)
+                    .show();
+        }
+    }
 }
